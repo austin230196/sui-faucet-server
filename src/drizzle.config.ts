191b9f5cbd732b259d86242a3e885bba5a7c6610
austin230196 <austinakamelu55@gmail.com>
2025-08-08 14:14:21 +0100

    Added CI-CD 003

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
-import config from './src/config/index.config';
+import config from './config/index.config';
 
 
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/model/*.ts',
+  out: '../drizzle',
+  schema: './model/*.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: `postgresql://${config.db.postgres.user}:${config.db.postgres.password}@${config.db.postgres.host}:${config.db.postgres.port}/${config.db.postgres.name}`,
